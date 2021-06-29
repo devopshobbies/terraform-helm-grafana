4e1b76b33e0197d82db93f2084e5c5505c481529
ahmadalibagheri <ahmadalibagheri@yahoo.com>
2021-06-29 18:25:11 +0100

    update version

--- versions.tf
+++ versions.tf
@@ -1,10 +1,13 @@
 terraform {
-  required_version = ">= 0.13.1"
-
   required_providers {
+    helm = {
+      version = "~> 2.4.1"
+    }
+    kubernetes = {
+      version = "~> 2.8.0"
+    }
     aws = {
-      source  = "hashicorp/aws"
-      version = ">= 3.0"
+      version = "~> 3.0.0"
     }
   }
 }
